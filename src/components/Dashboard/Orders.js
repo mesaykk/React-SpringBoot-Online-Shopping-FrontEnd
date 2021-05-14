38e10445c73b6d877a3d3f9b00f6a83eaad30f39
mesaykk <mesay.kabew@hotmail.com>
2021-05-13 19:55:08 -0700

    order imports ...

--- src/components/Dashboard/Orders.js
+++ src/components/Dashboard/Orders.js
@@ -1,11 +1,6 @@
 import React, {useContext}from 'react';
-<<<<<<< HEAD:src/components/Dashboard/SellerDashboard/Orders.js
-import {Table} from 'react-bootstrap';
-import {AllOrders} from '../../../store/AllOrders';
-=======
-import {Table, Dropdown, DropdownButton} from 'react-bootstrap';
-import {AllOrders} from '../../store/AllOrders';
->>>>>>> 0e15097f47025052598550228df56da639afe3b2:src/components/Dashboard/Orders.js
+
+
 
 
 
